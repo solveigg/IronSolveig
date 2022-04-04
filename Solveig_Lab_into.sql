c18d5a006f6c5f89ccc31a20364e521160697530
solveigg <52336172+solveigg@users.noreply.github.com>
2022-04-04 22:19:14 +0200

    with daniel

--- Solveig_Lab_into.sql
+++ Solveig_Lab_into.sql
@@ -1,3 +1,4 @@
+-- first SQL Lab
 use sakila;
 show tables from sakila;
 select title from film;
@@ -5,4 +6,6 @@ select distinct9name0 as langauge from langauge;
 select count(store_id) from store;
 select count(staff_id) from staff;
 select first_name from staff;
+
+
 
